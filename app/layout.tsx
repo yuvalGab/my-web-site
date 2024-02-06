9e251be6f9c297560ab0eb4c994beabc9e8a175c
Yuval Gabian <yuvalgab2007@gmail.com>
2024-02-06 12:37:56 +0200

    feat(layout): initial UI + create header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./(layout)/Header";
+import Footer from "./(layout)/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
     default: "My WEB Site",
-    template: "%s | My WEB Site"
+    template: "%s | My WEB Site",
   },
   description: "personal resume website about myself and my skills",
 };
@@ -19,7 +21,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="app-container">
+          <header>
+            <Header />
+          </header>
+          <main>{children}</main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
+      </body>
     </html>
   );
 }
