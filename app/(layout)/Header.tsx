d662cad22ab1247575a258a2956ebb01b46aa987
Yuval Gabian <yuvalgab2007@gmail.com>
2024-02-06 13:01:37 +0200

    feat(header): add navigation links

--- app/(layout)/Header.tsx
+++ app/(layout)/Header.tsx
@@ -1,3 +1,36 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { name: "Home", href: "/" },
+  { name: "Experience", href: "/experience" },
+  { name: "Education", href: "/education" },
+  { name: "Skills", href: "/skills" },
+];
+
 export default function Header() {
-  return <div className="header">header</div>;
+  const pathname = usePathname();
+
+  return (
+    <div className="header">
+      <p>My WEB Site</p>
+      {navLinks.map((link) => {
+        console.log(pathname);
+        const isActive =
+          (link.href === "/" && pathname === "/") ||
+          (link.href !== "/" && pathname.startsWith(link.href));
+
+        return (
+          <Link
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            key={link.name}
+            href={link.href}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
